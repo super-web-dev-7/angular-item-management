9e398adb76e94f658575373e9c7946d8e87e2aed
Yarden Gabay <gabay.yarden@gmail.com>
2020-06-10 17:15:23 +0300

    Remove modal component from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -96,7 +96,6 @@ export class AppHammerConfig extends HammerGestureConfig {
         RootLayout,
         BlankCorporateComponent,
         pgCardSocial,
-        ModalComponent,
     ],
     imports: [
         BrowserModule,
