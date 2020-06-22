73657662977265982a196551364e722a729b3384
supermanIT <supermanit0517@gmail.com>
2020-06-22 17:45:34 +0800

    fix Issue #68, #72

--- src/app/@pages/components/menu/menu.component.ts
+++ src/app/@pages/components/menu/menu.component.ts
@@ -1,89 +1,92 @@
-
-import { Component, OnInit,Input, ViewEncapsulation, HostListener, ViewChild } from '@angular/core';
+import {Component, OnInit, Input, ViewEncapsulation, HostListener, ViewChild} from '@angular/core';
 import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
+    animate,
+    state,
+    style,
+    transition,
+    trigger,
 } from '@angular/animations';
-import { PerfectScrollbarConfigInterface,
-  PerfectScrollbarComponent, PerfectScrollbarDirective } from 'ngx-perfect-scrollbar';
-import { CreateProjectComponent } from '@app/common/common-forms/create-project/create-project.component';
+import {
+    PerfectScrollbarConfigInterface,
+    PerfectScrollbarComponent, PerfectScrollbarDirective
+} from 'ngx-perfect-scrollbar';
+import {CreateProjectComponent} from '@app/common/common-forms/create-project/create-project.component';
 
 @Component({
-  selector: 'pg-menu-items',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
-  animations: [
-    trigger('toggleHeight', [
-        state('close', style({
-            height: '0',
-            overflow: 'hidden',
-            marginBottom:'0',
-            display:'none',
-           
-        })),
-        state('open', style({
-            display:'block',
-            height: '*',
-            marginBottom:'10px',
-            overflow: 'hidden',
-        })),
-        transition('close => open', animate('140ms ease-in')),
-        transition('open => close', animate('140ms ease-out'))
-    ])
-  ],
-  encapsulation:ViewEncapsulation.None 
+    selector: 'pg-menu-items',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss'],
+    animations: [
+        trigger('toggleHeight', [
+            state('close', style({
+                height: '0',
+                overflow: 'hidden',
+                marginBottom: '0',
+                display: 'none',
+
+            })),
+            state('open', style({
+                display: 'block',
+                height: '*',
+                marginBottom: '10px',
+                overflow: 'hidden',
+            })),
+            transition('close => open', animate('140ms ease-in')),
+            transition('open => close', animate('140ms ease-out'))
+        ])
+    ],
+    encapsulation: ViewEncapsulation.None
 })
 export class MenuComponent implements OnInit {
-  menuItems = [];
-  setupItems = [];
-  projectItems = [];
-  currentItem = null;
-  isPerfectScrollbarDisabled = false
-  public config: PerfectScrollbarConfigInterface = {};
-  constructor() { }
+    menuItems = [];
+    setupItems = [];
+    projectItems = [];
+    currentItem = null;
+    isPerfectScrollbarDisabled = false;
+    public config: PerfectScrollbarConfigInterface = {};
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
-  ngAfterViewInit() {
-    setTimeout(()=>{
-      this.togglePerfectScrollbar();
-    })
-  }
+    ngOnInit() {
+    }
 
-  @HostListener("window:resize", [])
-	onResize() {
-    this.togglePerfectScrollbar();
-	}
+    ngAfterViewInit() {
+        setTimeout(() => {
+            this.togglePerfectScrollbar();
+        });
+    }
 
-  togglePerfectScrollbar(){
-    this.isPerfectScrollbarDisabled = window.innerWidth < 1025
-  }
+    @HostListener('window:resize', [])
+    onResize() {
+        this.togglePerfectScrollbar();
+    }
 
-  @Input()
-  set Items(value) {
-    this.menuItems = value
-  }
+    togglePerfectScrollbar() {
+        this.isPerfectScrollbarDisabled = window.innerWidth < 1025;
+    }
 
-  @Input()
-  set Projects(value) {
-    this.projectItems = value;
-  }
+    @Input()
+    set Items(value) {
+        this.menuItems = value;
+    }
 
-  @Input()
-  set SetupItems(value) {
-    this.setupItems = value;
-  }
-  
-  toggleNavigationSub(event,item) {
-      event.preventDefault();
-      if(this.currentItem && this.currentItem != item){
-        this.currentItem["toggle"] = 'close';
-      }
-      this.currentItem = item;
-      item.toggle = (item.toggle === 'close' ? 'open' : 'close');
-  }
+    @Input()
+    set Projects(value) {
+        this.projectItems = value;
+    }
+
+    @Input()
+    set SetupItems(value) {
+        this.setupItems = value;
+    }
+
+    toggleNavigationSub(event, item) {
+        event.preventDefault();
+        if (this.currentItem && this.currentItem != item) {
+            this.currentItem['toggle'] = 'close';
+        }
+        this.currentItem = item;
+        item.toggle = (item.toggle === 'close' ? 'open' : 'close');
+    }
 }
--- src/app/projects/pagination/pagination.component.ts
+++ src/app/projects/pagination/pagination.component.ts
@@ -7,11 +7,47 @@ import {EventEmitterService} from '@app/event-emitter.service';
     styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnInit {
-    @Input() gridApi;
+    @Input() totalPage;
+    @Input() pageNo;
+    @Input() datainarry;
+    @Output() getLatestitem: EventEmitter<any> = new EventEmitter();
 
     constructor(private eventEmitterService: EventEmitterService) {
     }
 
     ngOnInit() {
+    }
+
+    moveToFirstPage() {
+        this.datainarry = false;
+        this.pageNo = 1;
+        this.getLatestitem.emit();
+        this.eventEmitterService.onPageChange(this.pageNo);
+    }
+
+    moveToLastPage() {
+        this.datainarry = false;
+        this.pageNo = this.totalPage;
+        this.getLatestitem.emit();
+        this.eventEmitterService.onPageChange(this.pageNo);
+    }
+
+    moveToNext() {
+        console.log('next');
+        this.datainarry = false;
+        if (this.pageNo < this.totalPage) {
+            this.pageNo = this.pageNo + 1;
+            this.getLatestitem.emit();
+            this.eventEmitterService.onPageChange(this.pageNo);
+        }
+    }
+
+    moveToPrevious() {
+        this.datainarry = false;
+        if (this.pageNo > 1) {
+            this.pageNo = this.pageNo - 1;
+            this.getLatestitem.emit();
+            this.eventEmitterService.onPageChange(this.pageNo);
+        }
     }
 }
